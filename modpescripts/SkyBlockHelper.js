02d8139965f264baca84089d289722e1ffcdfb3d
Weiipad <478735862@qq.com>
2016-02-11 00:54:53 +0800

    Added SkyBlockMap

---
+++ modpescripts/SkyBlockHelper.js
@@ -0,0 +1,4 @@
+//泥土合成
+Item.addShapedRecipe(3, 4, 0, ["   ", "dg ", "gd "], ["d", 3, 0, "g", 13, 0]);
+//Item.addShapedRecipe(3, 1, 0, ["ccc", "ccc", "ccc"], ["c", 173, 0]);
+//Item.addShapedRecipe(3, 1, 0, ["ccc", "ccc", "ccc"], ["c", 173, 0]);
